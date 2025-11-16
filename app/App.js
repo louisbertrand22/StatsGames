9ca17a143ef5cec0c58c3f14c9dfa6b4cda0fb26
louisbertrand22 <louisbert91@gmail.com>
2025-11-16 18:40:06 +0100

    fix: cast

--- app/App.js
+++ app/App.js
@@ -11,32 +11,9 @@ const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    // <AuthProvider>
-    //   <AppNavigator />
-    //   <StatusBar style="dark" />
-    // </AuthProvider>
-    // Temporarily disable auth context to debug navigation issues
-    <AuthProvider>
-      {/* <AppNavigator /> */}
-      {/* <StatusBar style="dark" /> */}
-      {/* <View >
-        <Text>StatsGames</Text>
-        <Text >Game Statistics with NFC</Text>
-        <ActivityIndicator 
-          size="large" 
-        />
-      </View> */}
-      <NavigationContainer>
-        <Stack.Navigator
-        // screenOptions={{
-        //   headerShown: false,
-        // }}
-      >
-        {/* <Stack.Screen name="Loading" component={LoadingScreen} /> */}
-      </Stack.Navigator>
-        
-      </NavigationContainer>
-    </AuthProvider>
-    
-  );
+      <AuthProvider>
+        <AppNavigator />
+        <StatusBar style="dark" />
+      </AuthProvider>
+    );
 }
